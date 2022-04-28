8e3018a9429a7f0b7aa04d5e098704d43f4ec334
Jonathan Tang <jonanthan2004@gmail.com>
2022-04-28 11:41:43 +0800

    New changes

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,39 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+
+def get_user_input():
+    nums = []
+    x = input()
+    x = x.split(",")
+
+    for stringedNums in x:
+        nums.append(float(stringedNums))
+    return nums
+
+
+def cal_average_temp(nums):
+    average = 0
+    for x in nums:
+        average = average + x
+    average = average / len(nums)
+    return average
+
+
+def calc_min_max_temperature(nums):
+    return [int(min(nums)), int(max(nums))]
+
+
+if __name__ == "__main__":
+    main()
+    display_main_menu()
+    lists = get_user_input()
+
+    print(cal_average_temp(lists))
+    print(calc_min_max_temperature(lists))
